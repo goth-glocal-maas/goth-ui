84d602818197405f2947875785390e5e66e129f0
sipp11 <sipp11@zzyzx.co>
2018-11-11 00:50:16 +0900

    Popup

---
+++ src/components/map/Popup.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react"
+import { Popup } from "react-map-gl"
+
+export default class MMarker extends Component {
+  render() {
+    const { lat = 7.8823, lon = 98.3817 } = this.props
+
+    return (
+      <Popup
+        latitude={lat}
+        longitude={lon}
+        closeButton={true}
+        closeOnClick={false}
+        anchor="bottom"
+      >
+        <div>You are here</div>
+      </Popup>
+    )
+  }
+}
+
+// ref: https://uber.github.io/react-map-gl/#/Documentation/api-reference/popup?section=properties
--- src/container/Map.js
+++ src/container/Map.js
@@ -2,11 +2,12 @@ import React, { Component } from "react"
 import ReactMapGL from "react-map-gl"
 import { fromJS } from "immutable"
 import styled from "styled-components"
+import axios from "axios"
 
 import Panel from "../components/Panel"
 import MMarker from "../components/map/Marker"
+import Popup from "../components/map/Popup"
 import "mapbox-gl/dist/mapbox-gl.css"
-
 const mapStyle = fromJS({
   style:
     "https://maps.tilehosting.com/styles/streets/style.json?key=W6SN2LHoDrAWj62wOyV0"
@@ -28,24 +29,63 @@ class Map extends Component {
       latitude: 7.8852323,
       longitude: 98.3808517,
       zoom: 13
+    },
+    defaultMapStyle: null,
+    mapStyle: null,
+  }
+
+  componentDidMount() {
+    axios
+      .get(process.env.REACT_APP_MAPGL_MAPSTYLE)
+      .then(resp => {
+        this.setState({ mapStyle: resp.data })
+        this._loadData()
+      })
+      .catch(error => {
+        console.error(error)
+      })
+    /*     requestJson('data/us-income.geojson', (error, response) => {
+      if (!error) {
+        this._loadData(response);
+      }
+    }); */
+  }
+
+  _loadData = data => {
+    let initMapStyle = this.state.defaultMapStyle
+    if (!data) {
+      this.setState({})
     }
+
+    // updatePercentiles(data, f => f.properties.income[this.state.year]);
+
+    // const mapStyle = defaultMapStyle
+    //   // Add geojson source to map
+    //   .setIn(['sources', 'incomeByState'], fromJS({type: 'geojson', data}))
+    //   // Add point layer to map
+    //   .set('layers', defaultMapStyle.get('layers').push(dataLayer));
+
+    // this.setState({data, mapStyle});
   }
 
   _onViewportChange = viewport => this.setState({ viewport })
 
   render() {
+    const { mapStyle } = this.state
     return (
       <FullPageBox>
         <Panel />
-
-        <ReactMapGL
-          {...this.state.viewport}
-          onViewportChange={this._onViewportChange}
-          mapStyle={process.env.REACT_APP_MAPGL_MAPSTYLE}
-          reuseMaps={true}
-        >
-          <MMarker mode="BUS" />
-        </ReactMapGL>
+        {mapStyle && (
+          <ReactMapGL
+            {...this.state.viewport}
+            onViewportChange={this._onViewportChange}
+            mapStyle={this.state.mapStyle}
+            reuseMaps={true}
+          >
+            <MMarker mode="BUS" />
+            <Popup />
+          </ReactMapGL>
+        )}
       </FullPageBox>
     )
   }
