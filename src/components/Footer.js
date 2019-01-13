b90c213a02681687940b1cdd9017bf7d2e408b3a
sipp11 <sipp11@zzyzx.co>
2019-01-14 02:57:10 +0900

    Add questionaire link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,23 +2,22 @@ import React from "react"
 import styled from "styled-components"
 import { version, date } from "../../package.json"
 import { gray } from "../constants/color"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 const StyledFooter = styled.span`
   font-size: 1.2rem;
   color: ${gray};
-
-  a {
-    color: ${gray};
-  }
 `
 
 const Footer = () => (
   <StyledFooter>
     v.{version}.{date}
-    &nbsp;&nbsp;
-    <a href="javascript:location.reload(true)">
-      <FontAwesomeIcon icon="sync" size="sm" />
+    <br />
+    <a
+      href="https://goo.gl/forms/XVXIynnUCBFJe2My1"
+      title="แบบสอบถามการใช้งาน GoTH"
+      target="_blank"
+    >
+      แบบสอบถามการใช้งาน GoTH
     </a>
   </StyledFooter>
 )
